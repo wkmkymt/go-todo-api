064964323fff2764aa7f0c94bf86ae88983bc67f
wkmkymt <wkmkymt@gmail.com>
2020-05-30 02:47:54 +0900

    [Fix] Todo Repository内のモデル名を修正

--- app/usecase/repository/todoRepository.go
+++ app/usecase/repository/todoRepository.go
@@ -4,5 +4,5 @@ import "github.com/wkmkymt/go-todo-api/domain/model"
 
 // TodoRepository is Todo Repository
 type TodoRepository interface {
-	FindAll() (model.Users, error)
+	FindAll() (model.Todos, error)
 }
