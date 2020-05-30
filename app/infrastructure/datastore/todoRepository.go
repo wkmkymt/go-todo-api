ed0bf9cd28363b550e2dde3fe77490fb5f40af49
wkmkymt <wkmkymt@gmail.com>
2020-05-30 15:05:40 +0900

    [Add] Todoの仮データを作成

--- app/infrastructure/datastore/todoRepository.go
+++ app/infrastructure/datastore/todoRepository.go
@@ -1,6 +1,10 @@
 package datastore
 
-import "github.com/wkmkymt/go-todo-api/domain/model"
+import (
+	"time"
+
+	"github.com/wkmkymt/go-todo-api/domain/model"
+)
 
 type todoRepository struct {
 }
@@ -16,7 +20,20 @@ func NewTodoRepository() TodoRepository {
 }
 
 func (ur *todoRepository) FindAll() (model.Todos, error) {
-	todos := model.Todos{}
+	todos := model.Todos{
+		{
+			ID:        "1",
+			Title:     "Todo 1",
+			Status:    true,
+			LimitDate: time.Now(),
+		},
+		{
+			ID:        "2",
+			Title:     "Todo 2",
+			Status:    false,
+			LimitDate: time.Now(),
+		},
+	}
 
 	return todos, nil
 }
