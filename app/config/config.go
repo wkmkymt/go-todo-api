85868cbb917829dae218e359ad19fae7c97bfefc
wkmkymt <wkmkymt@gmail.com>
2020-05-31 00:16:29 +0900

    [Add] Configファイルを作成

---
+++ app/config/config.go
@@ -0,0 +1,46 @@
+package config
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/spf13/viper"
+)
+
+type config struct {
+	Postgres struct {
+		DBMS   string
+		USER   string
+		PASS   string
+		HOST   string
+		PORT   string
+		DBNAME string
+	}
+}
+
+// C is Config Valiable
+var C config
+
+// LoadConfig is Loading Application Config
+func LoadConfig() {
+	Conf := &C
+
+	viper.SetConfigName("config")
+	viper.SetConfigType("yml")
+	viper.AddConfigPath(filepath.Join("config"))
+
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Println("[Error] Load Config Error!")
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	if err := viper.Unmarshal(&Conf); err != nil {
+		fmt.Println("[Error] Config Unmarshal Error!")
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
--- app/main.go
+++ app/main.go
@@ -3,17 +3,25 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 
+	"github.com/wkmkymt/go-todo-api/config"
 	"github.com/wkmkymt/go-todo-api/infrastructure/api/router"
 	"github.com/wkmkymt/go-todo-api/registry"
 )
 
 func main() {
+	// Load Config
+	config.LoadConfig()
+
+	// Get Gin App Engine
 	g := gin.Default()
 
+	// Get App Handler
 	r := registry.NewAppInteractor()
 	h := r.NewAppHandler()
 
+	// Set App Router
 	router.NewRouter(g, h)
 
+	// Run Server
 	g.Run(":5000")
 }
